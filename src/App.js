33ed72b32efebb3e466784781ae787fc6fac5399
Alvin Salgado <alvinsalgado01@gmail.com>
2022-06-10 23:40:30 -0700

    add react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
+
 function App() {
-  return <div></div>;
+  const [active, setActive] = useState(0);
+  return (
+    <Router>
+      <Navbar active={active} setActive={setActive} />
+    </Router>
+  );
 }
 
 export default App;
