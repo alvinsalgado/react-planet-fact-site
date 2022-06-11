52d7cf5cda66709db650f83fa33c1a0c4f47ef83
Alvin Salgado <alvinsalgado01@gmail.com>
2022-06-10 23:27:57 -0700

    add data for navbar

---
+++ src/utils/data.js
@@ -0,0 +1,43 @@
+const links = [
+  {
+    id: 1,
+    name: 'mercury',
+    url: '/mercury',
+  },
+  {
+    id: 2,
+    name: 'venus',
+    url: '/venus',
+  },
+  {
+    id: 3,
+    name: 'earth',
+    url: '/earth',
+  },
+  {
+    id: 4,
+    name: 'mars',
+    url: '/mars',
+  },
+  {
+    id: 5,
+    name: 'jupiter',
+    url: '/jupiter',
+  },
+  {
+    id: 6,
+    name: 'saturn',
+    url: '/saturn',
+  },
+  {
+    id: 7,
+    name: 'uranus',
+    url: '/uranus',
+  },
+  {
+    id: 8,
+    name: 'neptune',
+    url: '/neptune',
+  },
+];
+export default { links };
