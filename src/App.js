f2da2f9d3640afac4d3e72e439a58bb76dcae7a9
Alvin Salgado <alvinsalgado01@gmail.com>
2022-06-14 18:14:22 -0700

    add Routes to render different planets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Navbar } from './components';
+import Planet from './pages/Planet';
 
 function App() {
   const [active, setActive] = useState(0);
   return (
     <Router>
       <Navbar active={active} setActive={setActive} />
+      <Routes>
+        <Route exact path='/' element={<Navigate to='/mercury' />}></Route>
+        <Route exact path='/:name' element={<Planet active={active} />}></Route>
+      </Routes>
     </Router>
   );
 }
