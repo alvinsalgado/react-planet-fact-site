edb28bfe6c2d416ca62944aaae18e71dc24e8290
Alvin Salgado <alvinsalgado01@gmail.com>
2022-06-16 10:23:52 -0700

    update import components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import Navbar from './Navbar/Navbar';
 import Tabs from './Tabs/Tabs';
-
-export { Navbar, Tabs };
+import PlanetImage from './PlanetImage/PlanetImage';
+import PlanetInfo from './PlanetInfo/PlanetInfo';
+import PlanetFacts from './PlanetFacts/PlanetFacts';
+export { Navbar, Tabs, PlanetImage, PlanetInfo, PlanetFacts };
--- src/pages/Planet.js
+++ src/pages/Planet.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Tabs } from '../components';
+import { Tabs, PlanetImage, PlanetInfo, PlanetFacts } from '../components';
 import { data } from '../utils/index';
 const Planet = ({ active }) => {
   const [activeTab, setActiveTab] = useState('overview');
@@ -11,6 +11,9 @@ const Planet = ({ active }) => {
   return (
     <main className='grid-container'>
       <Tabs activeTab={activeTab} setActiveTab={setActiveTab} name={name} />
+      <PlanetImage activeTab={activeTab} name={name} images={images} />
+      <PlanetInfo activeTab={activeTab} name={name} overview={overview} structure={structure} geology={geology} />
+      <PlanetFacts rotation={rotation} revolution={revolution} radius={radius} temperature={temperature} />
     </main>
   );
 };
