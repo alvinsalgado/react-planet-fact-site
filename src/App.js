3f7cbbc5064df0a9eeffc559df3a0767224a0e24
Alvin Salgado <alvinsalgado01@gmail.com>
2022-06-28 21:48:40 -0700

    add framer motion props for Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
 import { Navbar } from './components';
 import Planet from './pages/Planet';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
+  const location = useLocation();
+
   const [active, setActive] = useState(0);
   return (
-    <Router>
+    <>
       <Navbar active={active} setActive={setActive} />
-      <Routes>
-        <Route exact path='/' element={<Navigate to='/mercury' />}></Route>
-        <Route exact path='/:name' element={<Planet active={active} />}></Route>
-      </Routes>
-    </Router>
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.key}>
+          <Route exact path='/' element={<Navigate to='/mercury' />}></Route>
+          <Route
+            exact
+            path='/:name'
+            element={<Planet active={active} />}
+          ></Route>
+        </Routes>
+      </AnimatePresence>
+    </>
   );
 }
 
--- src/pages/Planet.js
+++ src/pages/Planet.js
@@ -1,20 +1,67 @@
 import React, { useState } from 'react';
 import { Tabs, PlanetImage, PlanetInfo, PlanetFacts } from '../components';
 import { data } from '../utils/index';
+import { motion } from 'framer-motion';
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: { duration: 1.5 },
+  },
+  exit: {
+    opacity: 0,
+    scale: 1.5,
+
+    transition: { ease: 'easeInOut', duration: 1 },
+  },
+};
+
 const Planet = ({ active }) => {
   const [activeTab, setActiveTab] = useState('overview');
 
   const { planets } = data;
 
-  const { name, overview, structure, geology, rotation, revolution, radius, temperature, images } = planets[active];
+  const {
+    name,
+    overview,
+    structure,
+    geology,
+    rotation,
+    revolution,
+    radius,
+    temperature,
+    images,
+  } = planets[active];
 
   return (
-    <main className='grid-container'>
+    <motion.main
+      variants={containerVariants}
+      initial='hidden'
+      animate='visible'
+      exit='exit'
+      className='grid-container'
+    >
       <Tabs activeTab={activeTab} setActiveTab={setActiveTab} name={name} />
       <PlanetImage activeTab={activeTab} name={name} images={images} />
-      <PlanetInfo activeTab={activeTab} name={name} overview={overview} structure={structure} geology={geology} />
-      <PlanetFacts rotation={rotation} revolution={revolution} radius={radius} temperature={temperature} />
-    </main>
+      <PlanetInfo
+        activeTab={activeTab}
+        name={name}
+        overview={overview}
+        structure={structure}
+        geology={geology}
+      />
+      <PlanetFacts
+        rotation={rotation}
+        revolution={revolution}
+        radius={radius}
+        temperature={temperature}
+      />
+    </motion.main>
   );
 };
 
